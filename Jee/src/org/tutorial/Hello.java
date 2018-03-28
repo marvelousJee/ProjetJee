29cfbdc249cae2e53d619dfbc1116a6024ad9a23
marvelousJee <37833037+marvelousJee@users.noreply.github.com>
2018-03-28 10:53:01 +0200

    Add files via upload

---
+++ Jee/src/org/tutorial/Hello.java
@@ -0,0 +1,28 @@
+package org.tutorial;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet( name="Inscription", urlPatterns = "/test" )
+public class Hello extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Hello World yo</title></head>");
+        System.out.println("yo");
+        out.println("<body>");
+        out.println("<h1>Hello World yo!</h1>");
+        out.println("</body></html>");
+    }
+
+}
---
+++ Jee/web/hello.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java"%>
+<%@page import="java.util.Date"%>
+<%
+String myParameter=request.getParameter("param");
+Date now = new Date();
+%>
+
+<html>
+<head>
+<title>Bonjour la Bretagne</title>
+</head>
+<body>
+<h1>Bonjour la Bretagne</h1>
+<p>Valeur du param�tre : <%= myParameter %></p>
+<p>Date de la requ�te : <%= now.toString() %></p>
+
+
+</body>
+</html>
